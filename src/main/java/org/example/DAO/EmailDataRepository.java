065efe74fe7878a4a30b76cff35dc5e2db5b1bd6
4Siris <leonid_voynov@mail.ru>
2025-04-24 17:29:38 +0300

    feat: add main DAOs

---
+++ src/main/java/org/example/DAO/EmailDataRepository.java
@@ -0,0 +1,9 @@
+package org.example.DAO;
+
+import org.example.models.EmailData;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmailDataRepository extends JpaRepository<EmailData, Long> {
+}
---
+++ src/main/java/org/example/DAO/PhoneDataRepository.java
@@ -0,0 +1,9 @@
+package org.example.DAO;
+
+import org.example.models.PhoneData;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PhoneDataRepository extends JpaRepository<PhoneData, Long> {
+}
